20fddcb2537c4dbb8bc6f1ade671cc3b86f1ea67
blurryface <jose.barealpz@gmail.com>
2018-11-05 13:38:40 +0100

    prueba

--- HTML/JS/tests1.js
+++ HTML/JS/tests1.js
@@ -1,16 +1,5 @@
-let w = new Worker('./worker-test.js');
-
-console.log("Esto es el script");
-
-
 document.getElementById('res1').onclick = (function (evnt) {
-
-    // console.log(evnt);
-    evnt.preventDefault();
-
-    // document.querySelectorAll('.error').forEach(elem => {
-    //   elem.classList.add('esconder');
-    //});
+    // evnt.preventDefault();
 
     let formValid = document.getElementById('loginform');
 
@@ -27,28 +16,23 @@ document.getElementById('res1').onclick = (function (evnt) {
         }
     }
 
-    window.localStorage.getItem(`loginform`);
+    let respuestastest = [];
+    
+    respuestastest[0]=question1;
+
+    window.localStorage.setItem('respuestasTest', JSON.stringify(respuestastest));
     console.log(question1);
 
-
+    //[a,b,c,d,1]
 
     if (formValid) {
         //ENVIAR
         $.ajax({
             method: "POST",
             url: "http://www.mocky.io/v2/5bd9731d2f00005e0006d11d",
-
-            //url: "http:www.mocky.io/v2/5be0156b3200006100649400", //url false//
             data: question1,
         }).done(function () {
             console.log("La respuesta es " + question1);
-
-            //if (data.result) {
-            //  window.location.href = 'test2.html';
-            //} else {
-            //mensaje de error
-            //  document.getElementById('mensaje').classList.remove('esconder');
-            //}
         });
 
     }
