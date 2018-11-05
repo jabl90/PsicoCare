56abf7289ad86e97d02fccbb76e349be691e2675
Rebel1898 <Sergio.Llamas@protonmail.ch>
2018-11-05 17:04:44 +0100

    textareas como formularios

---
+++ HTML/JS/actividad2.js
@@ -0,0 +1,32 @@
+document.getElementById('submitboton').onclick = (function (evnt) {
+    
+ let formValid = document.getElementById('textform');
+ textarray=[];      
+ for (let index = 0; index < formValid.length-1; index++) {
+textarray[index]= formValid[index].value;
+
+ }
+          
+     if (formValid) {
+            //ENVIAR
+            $.ajax({
+                method: "POST",
+                url: "http://www.mocky.io/v2/5bd9731d2f00005e0006d11d",
+               
+                //url: "http:www.mocky.io/v2/5be0156b3200006100649400", //url false//
+            }).done(function () {
+                console.log(textarray)
+                console.log("Request sent");
+    
+                //if (data.result) {
+                  //  window.location.href = 'test2.html';
+                //} else {
+                    //mensaje de error
+                  //  document.getElementById('mensaje').classList.remove('esconder');
+                //}
+            });
+    
+        } 
+    
+    
+    });
